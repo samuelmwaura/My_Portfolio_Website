692686db34feb32aff8b4c2050402f0817d9231f
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-07 21:31:13 +0300

    Addition of Css styles

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-docume
+const profileSection = document.querySelector('#profile #webprogrammingStatement');
+const ruby = document.getElementById('ruby');
+const webProgramming= document.getElementById('webProgrammingImg');
+const webDesignImg = document.querySelector('#profile #webDesign');
+const webDevelopment = document.getElementById('webDevelopment');
+
+profileSection.addEventListener('mouseover',()=>{
+    ruby.style.display ='block';
+    webProgramming.style.display = 'none';
+});
+
+profileSection.addEventListener('mouseout',()=>{
+    ruby.style.display ='none';
+    webProgramming.style.display = 'block';
+});
+
+webDesignImg.addEventListener('mouseover',()=>{
+webDevelopment.style.display = 'block';
+webDesignImg.style.display = 'none';
+});
+
+webDesignImg.addEventListener('mouseout',()=>{
+webDesignImg.style.display = 'block';
+webDevelopment.style.display = 'none';
+});
